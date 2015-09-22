bc264f11de77a538418b4982e25a8a7f010fca03
Joschka <joschka@kintscher.me>
2015-09-22 14:59:38 +0200

    Add simple XHR wrapper for GET and POST requests

---
+++ src/lib/request.js
@@ -0,0 +1,23 @@
+function request(method, url, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.onreadystatechange = handler;
+  xhr.responseType = 'json';
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.send();
+
+  function handler() {
+    if (this.readyState === this.DONE) {
+      if (this.status === 200) {
+        success(this.response);
+      } else {
+        error(this);
+      }
+    }
+  };
+}
+
+export default {
+  get: request.bind(this, 'GET'),
+  post: request.bind(this, 'POST')
+};
