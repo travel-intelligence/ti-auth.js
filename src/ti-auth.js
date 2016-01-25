835c64e375f3849f6788abfd34401e05a24521c5
Joschka <joschka@kintscher.me>
2016-01-25 14:34:11 -0500

    Extract user retrieval for legibility

--- src/ti-auth.js
+++ src/ti-auth.js
@@ -1,6 +1,14 @@
 import Auth from './lib/authentication';
 import API from './lib/api';
 
+function retrieve_user(resolve) {
+  let token = Auth.token();
+  API.get(this.API_URL + '/api/v1/users/me',
+    resolve.bind(this, token),
+    Auth.unauthorize
+  );
+}
+
 export default {
   API_URL: '',
   DASHBOARD_URL: '',
@@ -9,11 +17,7 @@ export default {
       Auth.unauthorize();
       return;
     }
-    let token = Auth.token();
-    API.get(this.API_URL + '/api/v1/users/me',
-      resolve.bind(this, token),
-      Auth.unauthorize
-    );
+    retrieve_user.call(this, resolve);
   },
   signout() {
     Auth.unauthorize();
